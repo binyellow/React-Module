309652277f60222c7db7c30c7994a1c84404ab95
binyellow <591112995@qq.com>
2017-10-25 11:06:23 +0800

    add antd module

--- entry.js
+++ entry.js
@@ -1,6 +1,11 @@
 import {render} from 'react-dom'
 import React from 'react'
+import {Button,Icon} from 'antd';
+import 'antd/dist/antd.css';
+
 render(
-    <div>1</div>,
+    <div>
+        <Button className="pull-left"><Icon type="user" />   <a href="http://axuebin.com">薛彬</a></Button>
+    </div>,
     document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,10 @@ module.exports = {
   },
   module:{
     loaders:[
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } },
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] }},
+        { test: /\.css$/,loader: 'style-loader!css-loader'}
         // {test: /\.js$/,loader: 'babel?presets=react&&es2015'},
-        { test: /\.less$/, exclude: /node_modules/, loader: 'style-loader!css-loader!less-loader' },
+        // { test: /\.less$/, exclude: /node_modules/, loader: 'style-loader!css-loader!less-loader' },
         // { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader' },
         // { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
         // { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
